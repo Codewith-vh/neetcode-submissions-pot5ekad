fad6fc3a9fbaedffe38816b94c93268769ee0388
Codewith-vh <159415273+Codewith-vh@users.noreply.github.com>
2026-04-04 15:16:12 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=0, max=prices[prices.length-1];
+        for(int i=prices.length-2;i>=0;i--){
+            if(prices[i]>max){
+                max=prices[i];
+                continue;
+            }
+            if(min<max-prices[i]){
+                min=max-prices[i];
+            }
+        }
+        return min;
+    }
+}
