558e0b4142b6368564acbe1ab12e5af7c4e30e33
Codewith-vh <159415273+Codewith-vh@users.noreply.github.com>
2026-04-04 15:01:32 +0530

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, ArrayList<Integer>> hm= new HashMap<Integer, ArrayList<Integer>>();
+        boolean ans=false;
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                hm.get(nums[i]).add(i);
+            }
+            else{
+            hm.put(nums[i], new ArrayList<Integer>());
+            hm.get(nums[i]).add(i);
+            }
+        }
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : hm.entrySet()) {
+            ArrayList<Integer> value = entry.getValue();
+            if (value.size() > 1) {
+                int j=0,l=1;
+                while(l<value.size()){
+                    if (value.get(l) - value.get(j) <= k) {
+                        ans = true;
+                        break;
+                    }
+                    j++;
+                    l++;
+            }
+            }
+            if(ans){
+                break;
+            }
+        }
+        return ans;
+    }
+}
